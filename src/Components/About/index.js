d4ad3fb17409d13c71c649b76d4ecf6e6be62c45
Tricia Sawyer <tricia.sawyer03@gmail>
2023-07-20 17:05:26 -0500

    try to fix images

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import { Card, Image, Text } from '@mantine/core';
+import { Card, Image, MantineProvider, Text } from '@mantine/core';
 
 const teamMembersContent = [
     {
         name: 'Joshua Coffey',
-        imageSrc: '/assets/Josh.png',
+        imageSrc: './assets/Josh.png',
         information: 'I am a full-stack web developer attending Code Fellows. I have a passion for producing products that people love using and troubleshooting.'
     },
     {
         name: 'Kenya Womack',
-        imageSrc: '/assets/Kenya.png',
+        imageSrc: './assets/Kenya.png',
         information: ''
 
     },
     {
         name: 'Justin Mathieu',
-        imageSrc: '/assets/Justin.png',
+        imageSrc: './assets/Justin.png',
         information: 'Excited to begin a career in software development and build awesome things, while continuing to learn as much as possible!'
 
     },
     {
         name: 'Tricia Sawyer',
-        imageSrc: '/assets/Tricia.png',
+        imageSrc: './assets/Tricia.png',
         information: 'Full Stack Software Developer, furthering my knowledge through my education at DeltaV code school.'
 
     },
     {
         name: 'Tim Maupin',
-        imageSrc: '/assets/Tim.png',
+        imageSrc: './assets/Tim.png',
         information: ''
 
     },
     {
         name: 'Donna Ada',
-        imageSrc: '/assets/Donna.png',
+        imageSrc: './assets/Donna.png',
         information: 'Aspiring full-stack developer looking to make a difference one web application at a time || Currently learning at codefellows'
 
     },
@@ -55,18 +55,18 @@ const About = () => {
         );
     };
     
-    // return (
-        //   <MantineProvider>
-        //     <div>
-        //       <h1>About Our Team</h1>
-        //       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: 20 }}>
-        //         {teamMembersContent.map((member, index) => (
-                //   <TeamMembers key={index} {...member} />
-        //         ))}
-        //       </div>
-        //     </div>
-        //   </MantineProvider>
-    //   );
+    return (
+          <MantineProvider>
+            <div>
+              <h1>About Our Team</h1>
+              <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: 20 }}>
+                {teamMembersContent.map((member, index) => (
+                  <TeamMembers key={index} {...member} />
+                ))}
+              </div>
+            </div>
+          </MantineProvider>
+      );
     };
 
 
