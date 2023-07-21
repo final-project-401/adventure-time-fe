6399316b915d9ee4b2621b6fa973281c5a742146
Tricia Sawyer <tricia.sawyer03@gmail>
2023-07-21 13:05:38 -0500

    fix app js to correctly navigate

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Footer from './Components/Footer'
 import About from './Components/About';
 // import EventForm from './Components/EventForm';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 const App = () => {
 
   return (
-    <>
-    <Header />
-    {/* <EventForm /> */}
-    <About />
-    <Footer />
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        {/* <Route path="/" component={} /> */}
+        <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
