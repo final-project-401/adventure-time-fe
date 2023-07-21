cf9694917324171fc9d9806ada6c67004d7836ce
Tricia Sawyer <tricia.sawyer03@gmail>
2023-07-21 13:30:53 -0500

    add all routes besides weather for now

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import Header from './Components/Header'
-import Footer from './Components/Footer'
+import Home from './Components/AdventureHome';
+import Header from './Components/Header';
 import About from './Components/About';
-// import EventForm from './Components/EventForm';
-import './App.css';
+import EventItem from './Components/EventItem';
+import Contact from './Components/Contact';
+// import Weather from './Components/Weather';
+import Footer from './Components/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 
-
-const App = () => {
+function App() {
 
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        {/* <Route path="/" component={} /> */}
-        <Route path='/about' element={<About />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sales-info" element={<EventItem />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/place-order" element={<Weather />} /> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 };
 
