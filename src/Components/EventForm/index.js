bad61999b0639800bc835eaf0c232d1bfc163f05
Tim Maupin <timmaupin@live.com>
2023-07-24 10:50:40 -0500

    add in some code for the form

--- src/Components/EventForm/index.js
+++ src/Components/EventForm/index.js
@@ -7,3 +7,16 @@ function EventForm() {
 }
 
 export default EventForm
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('You clicked submit.');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
