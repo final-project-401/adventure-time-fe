b96b4df4b8915d012dfdaebabcb87945319e842f
Tricia Sawyer <tricia.sawyer03@gmail>
2023-07-24 12:43:57 -0500

    try to style header

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import StripWeather from '../Weather/stripWidget';
+// import Header from '../Header';
 
 export default function Hero({postcode}) {
   console.log('postcode>>>', postcode);
@@ -8,8 +9,10 @@ export default function Hero({postcode}) {
   return (
     <>
       <div className="hero">
+      {/* <Header /> */}
         <h1>Your next adventure starts here!</h1>
       </div>
+      
       {postcode
           ? <div className='stripWeather'><StripWeather postcode={postcode}/> </div>
           : ''
