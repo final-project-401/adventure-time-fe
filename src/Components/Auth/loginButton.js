80bcea320342516c154238c28121174a5c839d1c
Justin <justinmathiue0@gmail.com>
2023-07-24 11:31:23 -0700

    work on auth

--- src/Components/Auth/loginButton.js
+++ src/Components/Auth/loginButton.js
@@ -1,10 +1,28 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
-  return(
+
+  async function handleLogin(email) {
+    loginWithRedirect();
+    try {
+      if (isAuthenticated && user) {
+        let config = {
+          baseUrl: 'postgres://localhost:5432/adventure-time',
+          url: '/api/user',
+          method: 'post',
+          data: user.email,
+        }
+        const response = await axios.post(config)
+        console.log(response.data);
+      }
+    }
+  }
+
+  return (
     !isAuthenticated && (
-      <button onClick={() => loginWithRedirect()}>
+      <button onClick={() => handleLogin()}>
         Sign in
       </button>
     )
